1f0cfe8ab658c23df2dee8ab46f0016da4167e74
phillip18 <phillipjonathan@gmail.com>
2017-07-26 23:40:54 -0300

    Lesson 3 - Created models invitations and library, routes for admin and libraries. Created app in firebase and config the conection

--- config/environment.js
+++ config/environment.js
@@ -7,6 +7,22 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
+
+    // config/environment.js
+    firebase: {
+    apiKey: "AIzaSyBeJCRsK1E3_H9gTZb8mfB4OUkqHYzaesA",
+    authDomain: "library-app-4fce8.firebaseapp.com",
+    databaseURL: "https://library-app-4fce8.firebaseio.com",
+    storageBucket: "library-app-4fce8.appspot.com",
+  },
+
+  // if using ember-cli-content-security-policy
+    contentSecurityPolicy: {
+      'script-src': "'self' 'unsafe-eval' apis.google.com",
+      'frame-src': "'self' https://*.firebaseapp.com",
+      'connect-src': "'self' wss://*.firebaseio.com https://*.googleapis.com"
+    },
+
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
